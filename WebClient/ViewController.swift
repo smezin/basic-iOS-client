67a50e30e668282b5e7113f193c269cfe45f1c85
Shahaf Mezin <smezin@gmail.com>
2020-05-06 18:54:41 +0300

    Initial Commit

---
+++ WebClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebClient
+//
+//  Created by hyperactive on 06/05/2020.
+//  Copyright © 2020 hyperActive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
